ef9b403a0dbf83667807bb6197ad6424731bad2d
sachinbhoir4u <sachinbhoir4u@gmail.com>
2025-02-21 00:33:16 +0530

    PageNotFound component added

---
+++ src/pages/PageNotFound.jsx
@@ -0,0 +1,15 @@
+// import { Link } from "react-router-dom";
+
+const PageNotFound = () => {
+  return (
+    <div className="page-not-found">
+      <h1>404 - Page Not Found</h1>
+      <p>Oops! The page you're looking for doesn't exist.</p>
+      {/* <Link to="/" style={{ color: "blue", textDecoration: "underline" }}>
+        Go back to Home
+      </Link> */}
+    </div>
+  );
+};
+
+export default PageNotFound;
